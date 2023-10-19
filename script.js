8c1cd94b1c1b4fa6f034b5a11cebfc686eada4ab
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-19 19:40:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// - Getting started with objects
+
+// - key value pair
+let item = {
+  name:'smartphone',
+  price:50000,
+  quantity:1
+}
+console.log(item)
+
+// output -{name: 'smartphone', price: 50000, quantity: 1}
+
+// Another way to create object
+
+let item2 = new Object();
+item2.name = "iphone"
+item2.price = 90000
+item2.quantity = 1
+console.log(item2)
+
+// output - {name: 'iphone', price: 90000, quantity: 1}
